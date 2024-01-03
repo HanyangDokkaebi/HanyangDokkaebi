c09d61253b4f8d9d59c56fea4940bc08849eeb9a
jHyuk22 <jhlee4938@gmail.com>
2024-01-03 13:48:57 +0900

    feat(#3): 이동 시 플레이어가 이동 방향을 바라보는 함수 추가

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -18,13 +18,30 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // 현재 이동 방향 받아오기
         moveDirection = Input.GetAxisRaw("Horizontal");
+
+        // 이동하고, 방향 전환
         Move();
+        FlipDirection();
     }
 
     private void Move()
     {
-        //좌우이동
+        // 좌우 이동
         m_Rigidbody.velocity = new Vector2(moveDirection * speed, m_Rigidbody.velocity.y);
+    }
+
+    private void FlipDirection()
+    {
+        // 이동하는 방향에 따라 캐릭터 방향 전환
+        if (moveDirection > 0)
+        {
+            transform.localScale = new Vector3(-2, 2, 2); // 오른쪽으로 이동
+        }
+        else if (moveDirection < 0)
+        {
+            transform.localScale = new Vector3(2, 2, 2); // 왼쪽으로 이동
+        }
     }
 }
